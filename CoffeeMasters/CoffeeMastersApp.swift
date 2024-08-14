c1b6c87660fee9c72e8f76e57939bd0f8bb4c889
sheikh salman <salmanahmed@konasl.com>
2024-08-14 15:20:09 +0600

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by SK Salman Ahmad Sabbir on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMasters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeMasters
+//
+//  Created by SK Salman Ahmad Sabbir on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
